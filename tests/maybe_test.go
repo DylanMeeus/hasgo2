859b5229949db07fd9d61b9a13a191bd571bfb03
Dylan Meeus <meeusdylan@protonmail.com>
2022-04-30 16:57:42 +0200

    streamline test runner

--- tests/maybe_test.go
+++ tests/maybe_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/DylanMeeus/hasgo2/pkg"
 )
 
+type testTypes interface {
+    int | string
+}
+
 
 type maybeTest[T any] struct {
     m pkg.Maybe[T]
@@ -31,17 +35,53 @@ var (
 	    present: false,
 	},
     }
+
+
+    stringMaybeTests = []maybeTest[string] {
+	{
+	    m: pkg.Just("Hello World"),
+	    value: "Hello World",
+	    present: true,
+	},
+    }
 )
 
 func Test_Maybe(t *testing.T) {
-    for _, test := range intMaybeTests {
-	t.Run("", func(t *testing.T) {
+
+
+    /*
+
+    testRunner := func[T any] (test maybeTest[T], t *testing.T) {
 	    if test.present != test.m.Present {
 		t.Errorf("expected present to be %v but got %v\n", test.present, test.m.Present)
 	    }
 	    if test.value!= test.m.Value {
 		t.Errorf("expected value to be %v but got %v\n", test.value, test.m.Value)
 	    }
+	}
+
+	*/
+
+    for _, test := range intMaybeTests {
+	t.Run("", func(t *testing.T) {
+	    runTest(test, t)
 	})
     }
+
+
+    for _,test := range stringMaybeTests {
+	t.Run("", func(t *testing.T) {
+	    runTest(test, t)
+	})
+    }
+}
+
+
+func runTest[T testTypes](test maybeTest[T], t *testing.T) {
+	    if test.present != test.m.Present {
+		t.Errorf("expected present to be %v but got %v\n", test.present, test.m.Present)
+	    }
+	    if test.value!= test.m.Value {
+		t.Errorf("expected value to be %v but got %v\n", test.value, test.m.Value)
+	    }
 }
